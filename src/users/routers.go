7152d73b6ba1b7c9c7669cf0eb9157e041850e99
Manuel Valle <vallerem17@gmail.com>
2019-05-14 23:33:54 +0200

    user routes

---
+++ src/users/routers.go
@@ -0,0 +1,24 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	m "github.com/me/todo-go-server/src/models"
+	s "github.com/me/todo-go-server/src/shared"
+)
+
+func UsersRegistration(c *gin.Context) {
+	db := s.GetDB()
+	var user m.UserModel
+	c.BindJSON(&user)
+
+	password := user.PasswordHash
+	user.SetPassword(password)
+
+	if err := db.Save(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": err})
+	} else {
+		c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Account created successfully!", "user": user})
+	}
+}
